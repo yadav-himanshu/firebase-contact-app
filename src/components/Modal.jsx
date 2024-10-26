b9c48fae94ba4c61c61a42c9b82a42b95368f459
Himanshu Yadav <himanshuyadav7852@gmail.com>
2024-10-26 21:10:07 +0530

    modal styling fixed

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,15 +5,15 @@ const Modal = ({onClose, isOpen, children}) => {
   return createPortal(
     <>{
         isOpen&&(
-        <>
-            <div className='relative z-50 bg-white w-10/12 max-w-[370px] mx-auto min-h-fit p-3 flex flex-col rounded-md'>
+        <div className='absolute backdrop-blur h-screen z-40 top-0 w-screen grid place-items-center'>
+            <div className='relative z-50 bg-white w-full max-w-[370px]  mx-auto min-h-fit p-3 flex flex-col rounded-md'>
                 <CircleX onClick={onClose} className='ml-auto'/>
                 {children}
             </div>
             
             
-            <div onClick={onClose} className='absolute backdrop-blur h-screen z-40 top-0 w-screen'/>
-        </>)
+            <div onClick={onClose} />
+        </div>)
         
     }</>
   ,document.getElementById("modal-root"))
